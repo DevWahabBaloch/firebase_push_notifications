0d75f758f3e63dc1535d52cf28bf35a349e1a492
DevWahabBaloch <abdulwahabshafi001@gmail.com>
2025-01-05 22:52:07 +0500

    Handling Background messaging

--- lib/features/home_page/presentation/pages/home_page.dart
+++ lib/features/home_page/presentation/pages/home_page.dart
@@ -1,7 +1,16 @@
 import 'dart:developer';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:firebase_push_notification/core/services/notification_services.dart';
 import 'package:flutter/material.dart';
+
+// For Handling Background Messaging
+@pragma('vm:entry-point')
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  log('Handling a background message: ${message.notification!.title.toString()}');
+}
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_push_notification/features/home_page/presentation/pages/home_page.dart';
 import 'package:firebase_push_notification/my_app.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -8,5 +10,6 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
   runApp(const MyApp());
 }
